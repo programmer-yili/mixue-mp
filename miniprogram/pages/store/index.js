2ddf73110acf6bf5f1642dfeabc9a75e8baa0a4f
IlhamTahir <yantaq@bilig.biz>
2022-04-20 10:40:57 +0800

    day7 完成门店列表页面

--- miniprogram/pages/store/index.js
+++ miniprogram/pages/store/index.js
@@ -5,14 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    latitude: 0,
+    longitude: 0,
+    markers: [
+      {id: 1, 
+        title: '实力位置',
+         latitude:30.184871,
+          longitude:  120.264275,
+           iconPath: '../../assets/images/marker.png',
+          width: '55rpx', height: '69rpx'}
+    ]
   },
+
+  mapContext: null,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        this.setData({
+          latitude,
+          longitude
+        })
+      }
+     })
 
+
+    wx.createSelectorQuery().select('#store-map').context((res) => {
+      this.mapContext = res.context
+      }).exec()
+     
+  },
+
+  goToCurrentLocation() {
+    this.mapContext.moveToLocation()
   },
 
   /**
